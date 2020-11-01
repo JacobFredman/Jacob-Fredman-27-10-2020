992bc5be23577351caeff4244f63b0a13c9c942e
yacov fredman <jacov141@gmail.com>
2020-11-01 21:44:58 +0200

    flask running loacl

--- initApp.py
+++ initApp.py
@@ -1,6 +1,12 @@
-from flask import Flask
-from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
-app = Flask(__name__, static_folder='front-end/build', static_url_path='')
+from flask_cors import CORS, cross_origin
+from flask import Flask
+from flask import render_template
+from flask import send_from_directory
+from flask import request
+from flask import jsonify
+from flask import make_response
+
+# app = Flask(__name__)
+app = Flask(__name__, static_folder='front-end/prod', static_url_path='')
 cors = CORS(app)
