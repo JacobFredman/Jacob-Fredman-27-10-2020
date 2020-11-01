9a05b1fd26cd6267bdc862421d1db3d2ed935929
yacov fredman <jacov141@gmail.com>
2020-11-02 00:50:10 +0200

    cleaning

--- app.py
+++ app.py
@@ -1,26 +1,6 @@
 from initApp import *
-# from messagesView import *
+from messagesView import *
 
 
-@app.route('/api')
-@cross_origin()
-def Welcome():
-    return "Welcome to the API!!!"
-
-
-@app.route('/api/justpie/')
-@cross_origin()
-def GeneratePie():
-    'hiiiiiiiiiiiiiiiiiiiiiiiiiii!!!!!'
-
-
-@app.route('/')
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
-# if __name__ == "__main__":
-#     app.run(threaded=True)
+if __name__ == "__main__":
+    app.run(threaded=True)
