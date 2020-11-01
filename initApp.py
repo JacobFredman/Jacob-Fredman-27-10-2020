bffdaa12e9ae79d9399b8a084ef0e127179991a3
yacov fredman <jacov141@gmail.com>
2020-11-01 21:00:07 +0200

    dsss

--- initApp.py
+++ initApp.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
 # app = Flask(__name__, static_folder='front-end/build', static_url_path='')
 cors = CORS(app)
