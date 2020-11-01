a9b5b298a77bd653860764abb6c299fbba2a56bd
yacov fredman <jacov141@gmail.com>
2020-11-02 00:47:31 +0200

    revert git to the point before deploy

--- initApp.py
+++ initApp.py
@@ -1,12 +1,6 @@
-
-from flask_cors import CORS, cross_origin
 from flask import Flask
-from flask import render_template
-from flask import send_from_directory
-from flask import request
-from flask import jsonify
-from flask import make_response
+from flask_cors import CORS, cross_origin
 
-# app = Flask(__name__)
-app = Flask(__name__, static_folder='front-end/prod', static_url_path='')
+app = Flask(__name__)
+app = Flask(__name__, static_folder='front-end/build', static_url_path='')
 cors = CORS(app)
