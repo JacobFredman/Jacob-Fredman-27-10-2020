dab41e14b30e83a5b2eabe88f6b957b7ec09d0ab
yacov fredman <jacov141@gmail.com>
2020-11-01 20:46:50 +0200

    try server only

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ def GeneratePie():
     'hiiiiiiiiiiiiiiiiiiiiiiiiiii!!!!!'
 
 
-@app.route('/')
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
+# @app.route('/')
+# def serve():
+#     return send_from_directory(app.static_folder, 'index.html')
 
 
 if __name__ == '__main__':
--- front-end/src/utils/staticData.js
+++ front-end/src/utils/staticData.js
@@ -1,4 +1,5 @@
-export const baseUrl = "http://localhost:5000/";
+// export const baseUrl = "http://localhost:5000/";
 export const baseUrl = "http://home-assignment-jacob.herokuapp.com/api/";
+
 
 
