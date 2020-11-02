843abb284748fe1e0481cb9a2469b471c00e94d1
yacov fredman <jacov141@gmail.com>
2020-11-02 13:15:47 +0200

    base url change to aws

--- src/utils/staticData.js
+++ src/utils/staticData.js
@@ -1,5 +1,6 @@
-export const baseUrl = "http://localhost:5000/";
+// export const baseUrl = "http://localhost:5000/";
 // export const baseUrl = "http://home-assignment-jacob.herokuapp.com/api/";
+export const baseUrl = "http://homep10.us-west-2.elasticbeanstalk.com/";
 
 
 
